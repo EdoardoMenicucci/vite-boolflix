f7e0efe2d42f822ff8fad4cdee4241b1019e5155
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-05-29 19:59:17 +0200

    Sposto la funzione usata nell'header per trovare i film nello store per chiarezza e semplicita' di utilizzo

--- data/Store.js
+++ data/Store.js
@@ -5,6 +5,35 @@ let myData = reactive({
     modelUser: 'ritorno al futuro',
     stableSearch: '',
     filmsDetails:'',
+    // INSERISCO LA FUNZIONE DIRETTAMENTE NELLO STORE COSI CHE POSSA ESSERE RICHIAMATA DA TUTTI I COMPONENTI
+    getData() {
+        // CREO UNA VARIABILE STABILE E NON REACTIVE COME IL V-MODEL
+        let userSearch = this.modelUser
+        // GLI SPAZI NEL SERVER SONO INTERPRETATI CON %20 SPLITTO LA RICERCA DELL UTENTE E UNISCO L'ARRAY GENERATO CON %20
+        let userSearchPhp = userSearch.split(' ').join('%20')
+        this.stableSearch = userSearchPhp
+        // console.log(this.stableSearch);
+  
+        // OPZIONE NECESSARIA PER AVER ACCESSO AL SERVER
+        const options = {
+          method: 'GET',
+          url: 'https://api.themoviedb.org/3/search/movie?query=' + this.stableSearch + '&include_adult=false&language=it-IT&page=1',
+          headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0N2FmZDJhNDAxODc5ZWVhOGZjNjI1NWZkM2M4YmY3YiIsInN1YiI6IjY2NTcxZTU2MWE4ZjExZDYzM2Y4ZjdkOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xY2xUBVZ5Dda3gSmSCfG_Yld24zol4JJAwNNGA_OQKY'
+          }
+        };
+        // CHIAMATA AXIOS
+        axios
+          .request(options)
+          .then((response) => {
+            // console.log(response.data.results);
+            let risultato = response.data.results
+            // this.filmsDetails.push(risultato)
+            // console.log(this.filmsDetails);
+            this.filmsDetails = risultato
+          })
+      },
 })
 
 export default myData
--- src/App.vue
+++ src/App.vue
@@ -25,37 +25,10 @@ export default {
       console.log(this.store.filmsDetails);
       console.log(this.store.stableSearch);
     },
-    // QUA METODO PER COMUNICARE CON IL SERVER PHP E RECUPERARE DATI SEARCH
-    getData() {
-      let userSearch = this.store.modelUser
-      let userSearchPhp = userSearch.split(' ').join('%20')
-      this.store.stableSearch = userSearchPhp
-      // console.log(this.store.stableSearch);
-
-      // OPZIONE NECESSARIA PER AVER ACCESSO AL SERVER
-      const options = {
-        method: 'GET',
-        url: 'https://api.themoviedb.org/3/search/movie?query=' + this.store.stableSearch + '&include_adult=false&language=it-IT&page=1',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0N2FmZDJhNDAxODc5ZWVhOGZjNjI1NWZkM2M4YmY3YiIsInN1YiI6IjY2NTcxZTU2MWE4ZjExZDYzM2Y4ZjdkOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xY2xUBVZ5Dda3gSmSCfG_Yld24zol4JJAwNNGA_OQKY'
-        }
-      };
-      // CHIAMATA AXIOS
-      axios
-        .request(options)
-        .then((response) => {
-          // console.log(response.data.results);
-          let risultato = response.data.results
-          // this.store.filmsDetails.push(risultato)
-          // console.log(this.store.filmsDetails);
-          this.store.filmsDetails = risultato
-        })
-    }
   },
   mounted() {
 
-    this.getData();
+    this.store.getData();
     this.debug();
   },
 }
